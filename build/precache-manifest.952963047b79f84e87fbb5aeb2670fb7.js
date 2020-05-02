0daa3fe6fca842343432016bcdb212259f606447
Joseph Grigoletti <jgrig2@gmail.com>
2020-05-02 17:25:09 -0500

    reorg

---
+++ build/precache-manifest.952963047b79f84e87fbb5aeb2670fb7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63962dc475c8e313ef47e5127326bff8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec7fa3a760829f8a8e63",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b8148acb5bfc0607d5f8",
+    "url": "/static/js/2.471bf600.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.471bf600.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec7fa3a760829f8a8e63",
+    "url": "/static/js/main.e81757a9.chunk.js"
+  },
+  {
+    "revision": "252f969779de0734efb3",
+    "url": "/static/js/runtime-main.75211130.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
